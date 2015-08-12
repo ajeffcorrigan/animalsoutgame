eb13b47044b3488045ec89128a07b4230e0b7d27
Jeff Corrigan <ajeffcorrigan@yahoo.com>
2015-08-12 10:13:26 -0300

    Initial commit.

---
+++ desktop/src/com/ajeffcorrigan/game/animalsoutgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ajeffcorrigan.game.animalsoutgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ajeffcorrigan.game.animalsoutgame.AnimalsOutGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnimalsOutGame(), config);
+	}
+}
---
+++ html/src/com/ajeffcorrigan/game/animalsoutgame/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ajeffcorrigan.game.animalsoutgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ajeffcorrigan.game.animalsoutgame.AnimalsOutGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new AnimalsOutGame();
+        }
+}
