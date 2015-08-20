7e3e3bda95181c3df49fb0dbcdd071276dc8bfc7
Jeff Corrigan <ajeffcorrigan@yahoo.com>
2015-08-20 15:28:35 -0300

    Refactored game mechanics with states.

--- core/src/com/ajeffcorrigan/game/animalsoutgame/AnimalsOutGame.java
+++ core/src/com/ajeffcorrigan/game/animalsoutgame/AnimalsOutGame.java
@@ -1,40 +1,36 @@
 package com.ajeffcorrigan.game.animalsoutgame;
 
-import com.ajeffcorrigan.game.animalsoutgame.tools.GameScreenManager;
+import com.ajeffcorrigan.game.animalsoutgame.states.IntroState;
+import com.ajeffcorrigan.game.animalsoutgame.tools.GameStateManager;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class AnimalsOutGame extends ApplicationAdapter {
 	
+	
 	public static final boolean DEBUGON = true;			//Turn debugging on or off.
-	public OrthographicCamera camera;					
-	public SpriteBatch batch;
+	private SpriteBatch batch;
+	private GameStateManager gsm;
 	
 	public static int gh;								//Game height.
 	public static int gw;								//Game width.
-	
-	public GameScreenManager gsm;
-		
+			
 	public void create () {
 		//Initialize public objects and variables.
 		gw = Gdx.graphics.getWidth();					//Get graphics width.
 		gh = Gdx.graphics.getHeight();					//Get graphics height.
 		batch = new SpriteBatch();
-		
-		//Initialize and setup the camera.
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false,gw,gh);
+		gsm.push(new IntroState(gsm));
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 				
 	}
 	
 	public void render () {
-		
-	}
-	
-	static enum GameState {
-		title, activegame, pausegame, endgame
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 }
